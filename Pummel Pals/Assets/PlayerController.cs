af1333c2fb1bc45ad7415fc6401e4f0130b354e8
nandanvinjamury <nvinjamury@gmail.com>
2018-09-08 18:16:25 -0700

    set up basic player controls with no mechanics/skills

--- Pummel Pals/Assets/PlayerController.cs
+++ Pummel Pals/Assets/PlayerController.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	Rigidbody2D rb;
+	private Vector2 inputVector;
+	public float speed;
+	public float jumpForce;
+	private bool isGrounded;
+	public Transform groundCheck;
+	private float checkRadius;
+	public LayerMask groundDetect;
+	private float numberOfJumps;
+	public float extraJumps;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
+		numberOfJumps = extraJumps;
+		checkRadius = 0.05f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundDetect);
+
+		inputVector.x = Input.GetAxisRaw("Horizontal")*speed;
 		
+		if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && numberOfJumps > 0) {
+			inputVector.y = jumpForce;
+			numberOfJumps--;
+		} else if((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && numberOfJumps == 0 && isGrounded) {
+			inputVector.y = jumpForce;
+		} else {
+			inputVector.y = rb.velocity.y;
+		}
+
+		if (isGrounded) {
+			numberOfJumps = extraJumps;
+		}
+
+
+		rb.velocity = inputVector;
+		if(inputVector.x < 0) {
+			transform.localScale = new Vector2(-1,1);
+		} else if (inputVector.x > 0) {
+			transform.localScale = new Vector2(1, 1);
+		}
+
 	}
 }
